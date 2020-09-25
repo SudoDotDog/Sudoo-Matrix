2a48ba58af01b3d72de0078f1ed3c6ef7f2b6ba5
WMXPY <wm@mengw.io>
2020-09-25 11:09:18 -0500

    implementu tils

--- src/add.ts
+++ src/add.ts
@@ -4,7 +4,18 @@
  * @description Add
  */
 
-export const matrixAdd = (): void => {
+import { Matrix, MatrixSize } from "./declare";
+import { getMatrixSize } from "./util";
+
+export const matrixAdd = <T extends any = any>(left: Matrix<T>, right: Matrix<T>): Matrix<T> => {
+
+    const leftSize: MatrixSize = getMatrixSize(left);
+    const rightSize: MatrixSize = getMatrixSize(right);
+
+    if (leftSize.x !== rightSize.x
+        && leftSize.y !== rightSize.y) {
+        throw new Error("[Sudoo-Matrix] Only matrixes with same size can add");
+    }
 
     return;
 };
