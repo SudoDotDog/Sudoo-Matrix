f6224557c9c320dd01e9a8b5b88830412d8c0709
WMXPY <wm@mengw.io>
2020-09-24 19:22:11 -0500

    implement validate matrix function

--- src/util.ts
+++ src/util.ts
@@ -8,10 +8,34 @@ import { Matrix, MatrixSize } from "./declare";
 
 export const validateMatrix = (matrix: Matrix<any>): boolean => {
 
+    if (!Array.isArray(matrix)) {
+        return false;
+    }
 
+    const firstRow: any[] | undefined = matrix[0];
+    if (!Array.isArray(firstRow)) {
+        return false;
+    }
+
+    const columns: number = firstRow.length;
+    for (let i = 1; i < matrix.length; i++) {
+
+        const row: any[] | undefined = matrix[i];
+        if (!Array.isArray(row)) {
+            return false;
+        }
+
+        if (matrix[i].length !== columns) {
+            return false;
+        }
+    }
+    return true;
 };
 
 export const getMatrixSize = (matrix: Matrix<any>): MatrixSize => {
 
-
+    return {
+        x: 0,
+        y: 0,
+    };
 };
