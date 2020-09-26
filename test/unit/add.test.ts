4ce4666efb8af3e086cf656cc9f810853f6974d0
WMXPY <wm@mengw.io>
2020-09-26 15:59:34 -0500

    implement unite test for type check

--- test/unit/add.test.ts
+++ test/unit/add.test.ts
@@ -29,6 +29,42 @@ describe('Given [Add Matrix] function', (): void => {
         expect(runner).to.be.throw("[Sudoo-Matrix] Only matrixes with same size can add");
     });
 
+    it('should be able to throw when add illegal type', (): void => {
+
+        const left: Matrix<number> = [
+            [1, 2],
+            [3, 4],
+        ];
+        const right: Matrix<any> = [
+            [chance.string(), chance.string()],
+            [chance.string(), chance.string()],
+        ];
+
+        const runner: () => any = () => matrixAdd(left, right);
+
+        expect(runner).to.be.throw("[Sudoo-Matrix] Please provide addFunction for not-number or not-string value");
+    });
+
+    it('should be able to override add matrix function', (): void => {
+
+        const left: Matrix<number> = [
+            [1, 2],
+            [3, 4],
+        ];
+        const right: Matrix<any> = [
+            ['string', 'string'],
+            ['string', 'string'],
+        ];
+
+        // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+        const result: Matrix<number> = matrixAdd(left, right, (a: any, b: any) => a + b);
+
+        expect(result).to.be.deep.equal([
+            ["1string", "2string"],
+            ["3string", "4string"],
+        ]);
+    });
+
     it('should be able to add two matrix together', (): void => {
 
         const left: Matrix<number> = [
--- test/unit/minus.test.ts
+++ test/unit/minus.test.ts
@@ -29,6 +29,42 @@ describe('Given [Minus Matrix] function', (): void => {
         expect(runner).to.be.throw("[Sudoo-Matrix] Only matrixes with same size can minus");
     });
 
+    it('should be able to throw when minus illegal type', (): void => {
+
+        const left: Matrix<number> = [
+            [1, 2],
+            [3, 4],
+        ];
+        const right: Matrix<any> = [
+            [chance.string(), chance.string()],
+            [chance.string(), chance.string()],
+        ];
+
+        const runner: () => any = () => matrixMinus(left, right);
+
+        expect(runner).to.be.throw("[Sudoo-Matrix] Please provide minusFunction for not-number value");
+    });
+
+    it('should be able to override add matrix function', (): void => {
+
+        const left: Matrix<number> = [
+            [1, 2],
+            [3, 4],
+        ];
+        const right: Matrix<any> = [
+            ['string', 'string'],
+            ['string', 'string'],
+        ];
+
+        // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+        const result: Matrix<number> = matrixMinus(left, right, (a: any, b: any) => a + b);
+
+        expect(result).to.be.deep.equal([
+            ["1string", "2string"],
+            ["3string", "4string"],
+        ]);
+    });
+
     it('should be able to add two matrix together', (): void => {
 
         const left: Matrix<number> = [
